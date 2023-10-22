2e5aec6d7ad140c1bad713ac3deec2a6545718c1
ritika-jhanwar <ritika.jhanwar10@gmail.com>
2023-10-22 14:02:00 +0530

    final

--- src/components/wordCounter.js
+++ src/components/wordCounter.js
@@ -1,37 +1,28 @@
 import React from "react";
 import { useState } from "react";
+import "./word.css";
+
 export default function WordCounter() {
   const [text, setText] = useState("");
-  const containerStyle = {
-    width: "45%",
-    boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-    fontFamily: "Roboto Slab, serif",
-    fontSize: "1.3rem",
-  };
-  const headingStyle = {
-    fontSize: "3.5rem",
-  };
   const handleTextChange = (e) => {
     setText(e.target.value);
   };
   return (
-    <div className="container my-5 p-4" style={containerStyle}>
+    <div className="container my-5 p-4">
       <center>
-        <h1 className="mb-4" style={headingStyle}>
-          Responsive Paragraph Word Counter
-        </h1>
+        <h1 className="mb-4">Responsive Paragraph Word Counter</h1>
       </center>
       <center>
         <textarea
           name="form-control"
           id="counter"
-          cols="42"
+          cols="40"
           rows="8"
           value={text}
           onChange={handleTextChange}
         ></textarea>
       </center>
-      <p className="mt-4 mx-5 fs-3">
+      <p className="mt-4 mx-5">
         Word Count: {text ? text.split(" ").length : 0}
       </p>
     </div>
